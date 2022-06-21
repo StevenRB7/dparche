3d96f267bd5ad0af5ca0ede94f633601fea80f17
camila <mccotazo9@misena.edu.co>
2022-06-21 10:28:46 -0500

    categoria

--- src/views/Categorias/CategoriaView.vue
+++ src/views/Categorias/CategoriaView.vue
@@ -1,7 +1,39 @@
 <template>
+<!-- eslint-disable -->
   <div class="Categorias">
     <HeaderComponent/>
     <CarruselComponent/>
+    <div class="justify-content-center row">
+        <div class="col-8 card text-center ml-4 ml-sm-5 ml-lg-0">
+          <div class="card-body">
+     <h3 class="card-title mt-1 font-italic text-decoration-none " id="title-card">CATEGORIAS</h3>
+    <div class="col-8 card text-center ml-4 ml-sm-5 ml-lg-0" id="card-form">
+ <div class="form-group col-10 col-sm-8 offset-sm-2 col-lg-8 offset-lg-2">
+                        <select name="Categoria" class="custom-select m-2" required>
+                            <option value="">Tipo de Categoria</option>
+                            <option value="restaurante">Restaurante</option>
+                            <option value="bar">Bar</option>
+                            <option value="cultura">Cultural</option>
+                            <option value="discoteca">Dicoteca</option>
+                            <option value="motel">Motel</option>
+                        </select>
+                      </div>
+                      <form class="">
+                          <div class="form-group col-10 col-sm-8 offset-sm-2 col-lg-8 offset-lg-2">
+                            <textarea type="text" name="descripcion" maxlength="250" class="form-control m-2" placeholder="Descripción del Categoria Elegida" required></textarea>
+                          </div>
+
+                          
+
+
+                          <button type="submit" class="btn btn-primary">Enviar </button>
+                      </form>
+               
+          </div>
+        </div>  
+      </div>
+      </div>
+      <em><h4 class="bg-warning card-title mt-1 font-italic text-decoration-none " id="title-card">Mereces un descanso cuando las cosas estan dificiles..!!</h4></em>
   </div>
 </template>
 
