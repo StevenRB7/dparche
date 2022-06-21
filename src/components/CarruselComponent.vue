ef048623e1ed710b0728d13bfbfdb27166955da3
StevenRB7 <estarosero@misena.edu.co>
2022-06-21 14:01:19 -0500

    Carrusel Update

--- src/components/CarruselComponent.vue
+++ src/components/CarruselComponent.vue
@@ -1,55 +1,65 @@
 <template>
-  <div>
+  <div >
     <b-carousel
       id="carousel-1"
-      :interval="1500"
-      background="#c2c2c2"
+      :interval="3000"
+      background="#fffff"
       img-width="1024"
-      img-height="100"
+      img-height="200"
       style="text-shadow: 1px 1px 2px ;"
     >
-      <b-carousel-slide
-         id="carousel-1"
-        caption="-_-"
-        text="TOca arreglar."
-        img-src="../assets/deparche3.png"
-        img-width="1024"
-      img-height="100"
-      ></b-carousel-slide>
-
-      <b-carousel-slide
-      img-src="../assets/dparche.png"
-      img-width="1024"
-      img-height="100">
-
-        <h1>Carrusel!</h1>
-      </b-carousel-slide>
-
-      <b-carousel-slide
-      img-src="../assets/dparcheotro.png"
-      img-width="1024"
-      img-height="100">
-      </b-carousel-slide>
-
       <b-carousel-slide>
         <template #img>
           <img
-            width="1024"
-            height="100"
-            src="../assets/logoparche.png"
+            :interval="2500"
+            width="1240"
+            height="200"
+            src="../assets/ciclismo.jpg"
+          >
+        </template>
+      </b-carousel-slide>
+      <b-carousel-slide>
+        <template #img>
+          <img
+            :interval="2500"
+            width="1240"
+            height="200"
+            src="../assets/piscina.jpg"
+          >
+        </template>
+      </b-carousel-slide>
+      <b-carousel-slide>
+        <template #img>
+          <img
+            :interval="2500"
+            width="1240"
+            height="200"
+            src="../assets/tomar.jpg"
+          >
+        </template>
+      </b-carousel-slide>
+      <b-carousel-slide>
+        <template #img>
+          <img
+            :interval="2500"
+            width="1240"
+            height="200"
+            src="../assets/naturaleza.jpg"
+          >
+        </template>
+      </b-carousel-slide>
+            <b-carousel-slide>
+        <template #img>
+          <img
+            :interval="1500"
+            width="1240"
+            height="200"
+            src="../assets/fiestabanner.jpg"
           >
         </template>
       </b-carousel-slide>
 
-      <b-carousel-slide
-      class="d-block img-fluid w-100"
-      caption="Blank Image"
-      img-width="1024"
-      src="../assets/logoparche.png"
-      img-height="100">
-
-      </b-carousel-slide>
-    </b-carousel>
+  </b-carousel>
   </div>
 </template>
 
